d13ab024fa20f513a9110f725f80dcc61bf9ceb4
aniket <aniketkoli17884@gmail.com>
2024-08-14 10:31:12 +0530

    Add login endpoint

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -83,9 +83,37 @@ export const verifyEmail = async ( req , res  ) => {
 }
 
 export const login = async ( req , res ) => {
-    res.send("login");
+    const { email, password } = req.body
+
+    try {
+        const user = await User.findOne({ email: email})
+        
+        if (!user) return res.status(404).json({success: false, message:"User not found"})
+
+        const isPasswordValid = await bcryptjs.compare(password, user.password)
+        
+        if (!isPasswordValid) return res.status(401).json({success: false, message:"Invalid credentials"})
+
+        generateTokenAndSetCookie(res, user._id)
+
+        user.lastLogin = new Date()
+        await user.save()
+
+        res.status(200).json({
+            success: true,
+            message: "Logged in successfully",
+            user: {
+                ...user._doc,
+                password: undefined
+            }
+        })
+    } catch (error) {
+        console.log("Error in login", error)
+        res.status(400).json({success: false, message: error.message})
+    }
 }
 
 export const logout = async ( req , res ) => {
-    res.send("logout");
+    res.clearCookie("token")
+    res.status(200).json({success:false, message:"Logged out successfully"})
 }
